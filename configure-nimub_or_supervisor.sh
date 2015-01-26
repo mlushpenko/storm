29ddc90d8149a083d90a372e0fef955f0f91ce14
mlushpenko <iviakciivi@gmail.com>
2015-01-26 14:34:53 +0100

    Create configure-nimub_or_supervisor.sh

---
+++ configure-nimub_or_supervisor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#configure node
+echo 'storm.zookeeper.servers:' >> apache-storm-0.9.3/conf/storm.yaml
+echo "    - \"$ZOOKEEPER_IP\"" >> apache-storm-0.9.3/conf/storm.yaml
+echo "nimbus.host: \"$NIMBUS_IP\"" >> apache-storm-0.9.3/conf/storm.yaml
+echo 'storm.local.dir: "/mnt/storm"' >> apache-storm-0.9.3/conf/storm.yaml
+export STORM_LOCAL_DIR=/mnt/storm
+mkdir $STORM_LOCAL_DIR
+chmod -R 755 $STORM_LOCAL_DIR
+
+sudo supervisord -c supervisord.conf
