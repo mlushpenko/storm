e1a94375cf2086a6b7f3db24dca9db3c34740bfc
mlushpenko <iviakciivi@gmail.com>
2015-02-09 10:43:04 +0100

    Hostname -f is not always set properly, so change to IP

--- storm_config.sh
+++ storm_config.sh
@@ -5,7 +5,7 @@ echo 'storm.zookeeper.servers:' >> apache-storm-0.9.3/conf/storm.yaml
 echo "    - \"$ZOOKEEPER_IP\"" >> apache-storm-0.9.3/conf/storm.yaml
 echo "nimbus.host: \"$NIMBUS_IP\"" >> apache-storm-0.9.3/conf/storm.yaml
 echo 'storm.local.dir: "/mnt/storm"' >> apache-storm-0.9.3/conf/storm.yaml
-echo "storm.local.hostname: \"$(hostname -f)\"" >> apache-storm-0.9.3/conf/storm.yaml
+echo "storm.local.hostname: \"$LOCAL_IP\"" >> apache-storm-0.9.3/conf/storm.yaml
 export STORM_LOCAL_DIR=/mnt/storm
 mkdir $STORM_LOCAL_DIR
 chmod -R 755 $STORM_LOCAL_DIR
